96c6378ce23b02b616b47a7bff2c910a80182c72
erikia <nobileisabelle.in@gmail.com>
2022-08-15 15:36:19 +0200

    Soutenance

--- optimized.py
+++ optimized.py
@@ -4,74 +4,84 @@ start_time = datetime.now()
 
 
 class Greedy:
-    def clean_list(actions_list):
-        cleaned_list = [x for x in actions_list if float(x[1]) > 0]
+    """Greedy approach who calculate the ratio value/weight for each item and sort the item on basis of this ratio"""
+
+    def clean_list(shares_list):
+        """Clean the list by removing the price = or < 0"""
+        cleaned_list = [x for x in shares_list if float(x[1]) > 0]
         return cleaned_list
 
-    def turn_csv_into_actions_list(csv_file):
-        actions_list = []
+    def return_shares_list(csv_file):
+        """Read the CSV file and turn it into a list"""
+        shares_list = []
         with open(csv_file, newline='') as csvfile:
             spamreader = csv.reader(csvfile, delimiter=',')
             for row in spamreader:
-                actions_list.append(row)
-            actions_list.remove(actions_list[0])
-        return actions_list
+                shares_list.append(row)
+            shares_list.remove(shares_list[0])
+        return shares_list
 
-    def sorted_list_by_perfermance(actions_list):
+    def sorted_list_by_perfermance(shares_list):
         """Sort all the items in decreasing order of the ratio"""
         sorted_list = sorted(
-            actions_list, key=lambda x: float(x[2]), reverse=True)
+            shares_list, key=lambda x: float(x[2]), reverse=True)
         return sorted_list
 
-    def get_sorted_actions_list(csv_file):
-        dataset = Greedy.turn_csv_into_actions_list(csv_file)
+    def get_sorted_shares_list(csv_file):
+        """Return the shares list cleaned and sorted"""
+        dataset = Greedy.return_shares_list(csv_file)
         cleaned_list = Greedy.clean_list(dataset)
         sorted_list = Greedy.sorted_list_by_perfermance(cleaned_list)
         return sorted_list
 
-    def get_best_invest(actions_list, wallet, action_wallet):
-        for action in actions_list:
-            if wallet > 0 and wallet > float(action[1]):
-                action_wallet.append(action)
-                wallet -= float(action[1])
-        return action_wallet
+    def calculate_profitable_investment(shares_list, wallet, share_wallet):
+        """Return the list of the best calculated investment"""
+        for share in shares_list:
+            if wallet > 0 and wallet > float(share[1]):
+                share_wallet.append(share)
+                wallet -= float(share[1])
+        return share_wallet
 
     def return_price(combinaison):
+        """return the calculated price in the combination"""
         total_price = 0
-        for action in combinaison:
-            price = float(action[1])
+        for share in combinaison:
+            price = float(share[1])
             total_price += price
         return total_price
 
-    def return_benefice(combinaison):
+    def return_profit(combinaison):
+        """return the calculated profit in the combination"""
         total_benefits = 0
-        for action in combinaison:
-            price = float(action[1])
-            benefits_percent = float(action[2])
+        for share in combinaison:
+            price = float(share[1])
+            benefits_percent = float(share[2])
             benefit = price * benefits_percent / 100
             total_benefits += benefit
         return total_benefits
 
-    def main(actions_list):
-        print(f"Nombres d'actions achetées: {len(actions_list)}")
-        print("Liste Nom / Prix / Bénéfice en %: ")
-        print(actions_list)
-        print(f"Prix: {Greedy.return_price(actions_list)}")
-        print(f"Bénéfice: {Greedy.return_benefice(actions_list)}")
+    def main(shares_list):
+        """Print total cost, total return, shares list"""
+        print(f"Number of shares purchased: {len(shares_list)}")
+        print("Shares name / Total cost / Total return in %: ")
+        print(shares_list)
+        print(f"Total cost: {Greedy.return_price(shares_list)}")
+        print(f"Total return: {Greedy.return_profit(shares_list)}")
 
 
 if __name__ == "__main__":
-    dataset1 = Greedy.get_sorted_actions_list("csv/dataset1_Python+P7.csv")
-    dataset2 = Greedy.get_sorted_actions_list("csv/dataset2_Python+P7.csv")
-    action_wallet1 = []
-    action_wallet2 = []
+    dataset1 = Greedy.get_sorted_shares_list("csv/dataset1_Python+P7.csv")
+    dataset2 = Greedy.get_sorted_shares_list("csv/dataset2_Python+P7.csv")
+    share_wallet1 = []
+    share_wallet2 = []
     wallet = 500
 
-    actions_list1 = Greedy.get_best_invest(dataset1, wallet, action_wallet1)
-    actions_list2 = Greedy.get_best_invest(dataset2, wallet, action_wallet2)
-    Greedy.main(actions_list1)
-    Greedy.main(actions_list2)
+    shares_list1 = Greedy.calculate_profitable_investment(
+        dataset1, wallet, share_wallet1)
+    shares_list2 = Greedy.calculate_profitable_investment(
+        dataset2, wallet, share_wallet2)
+    Greedy.main(shares_list1)
+    Greedy.main(shares_list2)
 
     end_time = datetime.now()
     print('Duration: {}'.format(end_time - start_time))
-
